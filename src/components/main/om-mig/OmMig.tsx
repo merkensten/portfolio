2e7854ab88f4cc00f62c8925d2e5a2d0a555ff8c
Mattis <m.erkensten@icloud.com>
2022-09-14 15:48:04 +0200

    Fortsatt med arbetet av portfolion

--- src/components/main/om-mig/OmMig.tsx
+++ src/components/main/om-mig/OmMig.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { Wrapper } from '../../layout';
+import styles from './OmMig.module.scss';
 
-function OmMig() {
-  return <div>OmMig</div>;
+type Props = {
+  title: string;
+  text1: string;
+  text2: string;
+  imgUrl: string;
+  imgAlt: string;
+};
+
+function OmMig({ title, text1, text2, imgUrl, imgAlt }: Props) {
+  return (
+    <Wrapper cssClass={styles.om_mig_wrapper}>
+      <h2 className={styles.om_mig_title}>{title}</h2>
+
+      <div className={styles.om_mig_content_wrapper}>
+        <img className={styles.om_mig_img} src={imgUrl} alt={imgAlt} />
+        <div className={styles.om_mig_content_wrapper_text}>
+          <p>{text1}</p>
+          <p>{text2}</p>
+        </div>
+      </div>
+    </Wrapper>
+  );
 }
 
 export default OmMig;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import { SEO } from '../components/seo';
 import styles from '../styles/pages/Home.module.scss';
-import { Hero } from '../components/main';
+import { Hero, OmMig } from '../components/main';
 
 const Home: NextPage = () => {
   return (
@@ -21,6 +21,13 @@ const Home: NextPage = () => {
           subTitle="Jag är en JavaScript / TypeScript utvecklare med primärt fokus på Front End."
           imgUrl="/bilder/mattis/mattis-hero-bild.png"
           imgAlt="Mattis Portfolio Hero bild"
+        />
+        <OmMig
+          title="Om Mig"
+          text1="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce et volutpat elit. Pellentesque lectus nisl, molestie sed lorem quis, pretium sagittis dolor. Sed euismod, nisl eget aliquam lacinia, nunc nisl aliquam nisl, eget aliquam nisl nunc eget nisl. Sed euismod, nisl eget aliquam lacinia, nunc nisl aliquam nisl, eget aliquam nisl nunc eget nisl."
+          text2="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce et volutpat elit. Pellentesque lectus nisl, molestie sed lorem quis, pretium sagittis dolor. Sed euismod, nisl eget aliquam lacinia, nunc nisl aliquam nisl, eget aliquam nisl nunc eget nisl. Sed euismod, nisl eget aliquam lacinia, nunc nisl aliquam nisl, eget aliquam nisl nunc eget nisl."
+          imgUrl="/bilder/mattis/mattis-bild.png"
+          imgAlt="Mattis Portfolio Om Mig bild"
         />
         {/* 
         <img
